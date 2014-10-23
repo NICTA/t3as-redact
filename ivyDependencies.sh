8817f29cfa57eeaac418aae41a025497f6b63aa7
neilbacon <neil.bacon@nicta.com.au>
2014-10-24 09:27:17 +1100

    prepare release

--- ivyDependencies.sh
+++ ivyDependencies.sh
@@ -3,13 +3,15 @@
 deps() {
   awk 'NF == 3 && split($3, a, ":") == 3 {
     sub(/\x1B.*/, "", a[3])
-    printf "~/.ivy2/cache/%s/%s/srcs/%s-%s-sources.jar\n", a[1], a[2], a[2], a[3]  
-  }' | while read f; do
-    eval g=$f
-    if [[ -f $g ]]; then
-      echo $g
+    printf "~/.ivy2/cache/%s/%s/srcs/%s-%s-sources.jar ~/.ivy2/local/%s/%s/%s/srcs/%s-sources.jar\n", a[1], a[2], a[2], a[3], a[1], a[2], a[3], a[2]  
+  }' | while read c l; do
+    eval c=$c l=$l
+    if [[ -f $l ]]; then
+      echo $l
+    elif [[ -f $c ]]; then
+      echo $c
     else
-      echo $g >&2
+      echo $l >&2
     fi 
   done
 }
@@ -23,6 +25,6 @@ else
   deps < a0 > a1 2> a2                # convert dependencies to path under ~/.ivy2/cache, those found to a1, not found to a2
   cat a2                              # inspect for missing sources
                                       # tar up those found
-  tar -cvf target/dependencySourceJars.tar --transform 's,^.*/\.ivy2/cache/,,' `cat a1`
+  tar -cvf target/dependencySourceJars.tar --transform 's,^.*/\.ivy2/,,' `cat a1`
   rm a0 a1 a2
 fi
